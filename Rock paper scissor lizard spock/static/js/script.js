a795550bca81e5e74d462a346086f57bcc7dcd48
Dev Manek <manekdev2001@gmail.com>
2020-10-31 16:34:19 +0530

    Added spock and lizard

--- Rock paper scissor lizard spock/static/js/script.js
+++ Rock paper scissor lizard spock/static/js/script.js
@@ -2,8 +2,8 @@ function game(choice) {
   var humanchoice, botchoice;
   humanchoice = choice.id;
   botchoice = numtochoice(randint());
-  //console.log(humanchoice);
-  //console.log(botchoice);
+  console.log(humanchoice);
+  console.log(botchoice);
   result = decidewinner(humanchoice, botchoice);
   console.log(result);
   message = finalMessage(result);
@@ -13,23 +13,28 @@ function game(choice) {
     location.reload();
   }, 2000);
 }
-
+// Function that generated random numbers from 0-5
 function randint() {
-  return Math.floor(Math.random() * 3);
+  return Math.floor(Math.random() * 5);
 }
+// function converts the numbers to choices
 function numtochoice(number) {
-  return ["rock", "paper", "scissor"][number];
+  return ["rock", "paper", "scissor", "lizard", "spock"][number];
 }
+//function that decides the winner
 function decidewinner(yourchoice, compchoice) {
   var setrps = {
-    rock: { scissor: 1, rock: 0.5, paper: 0 },
-    scissor: { scissor: 0.5, rock: 0, paper: 1 },
-    paper: { scissor: 0, rock: 1, paper: 0.5 },
+    rock: { scissor: 1, rock: 0.5, paper: 0 ,lizard:1,spock:0},
+    scissor: { scissor: 0.5, rock: 0, paper: 1,lizard:1,spock:0 },
+    paper: { scissor: 0, rock: 1, paper: 0.5,lizard:0,spock:1 },
+    lizard: { scissor: 0, rock: 0, paper: 1,lizard:0.5,spock:1 },
+    spock: { scissor: 1, rock: 1, paper: 0,lizard:0,spock:0.5 }
   };
   var yourscore = setrps[yourchoice][compchoice];
   var compscore = setrps[compchoice][yourchoice];
   return [yourscore, compscore];
 }
+// function displaying final message
 function finalMessage([yourscore, compscore]) {
   if (yourscore == 0) {
     return { message: "You lost", color: "red" };
@@ -39,16 +44,21 @@ function finalMessage([yourscore, compscore]) {
     return { message: "You won", color: "green" };
   }
 }
-
+// Final function that does the job of printing it out for you
 function Frontendrps(humanchoice, botchoice, finalMessage) {
   var imagesDatabase = {
     rock: document.getElementById("rock").src,
     scissor: document.getElementById("scissor").src,
     paper: document.getElementById("paper").src,
+    lizard: document.getElementById("lizard").src,
+    spock: document.getElementById("spock").src,
+
   };
   document.getElementById("rock").remove();
   document.getElementById("scissor").remove();
   document.getElementById("paper").remove();
+  document.getElementById("lizard").remove();
+  document.getElementById("spock").remove();
 
   var human = document.createElement("div");
   var message = document.createElement("div");
